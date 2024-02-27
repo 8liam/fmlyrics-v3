c08498db28442b6ba96ad4507662b53a0df0cdb8
Liam <n11102764@qut.edu.au>
2024-02-28 01:42:20 +1000

    simplifying api route for deployment.

--- app/api/lyrics/route.ts
+++ app/api/lyrics/route.ts
@@ -11,45 +11,42 @@ type options = {
 };
 
 export async function GET(request: Request) {
-  try {
-    const client = new Client();
-    const url = new URL(request.url);
-    const title = url.searchParams.get("title");
-    const artist = url.searchParams.get("artist");
-    if (title && artist && title.length > 0 && artist.length > 0) {
 
-      const searches = await client.songs.search(
-        `${decodeURIComponent(title as string)} ${decodeURIComponent(
-          title.length > 1 ? (artist as string) : ""
-        )}`
-      );
-      const song = searches[0];
-      const lyrics = await song?.lyrics();
+  const client = new Client();
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title");
+  const artist = url.searchParams.get("artist");
+  if (title && artist && title.length > 0 && artist.length > 0) {
 
-      const response = NextResponse.json({
-        lyrics: lyrics,
-        title: song?.title,
-        artist: song?.artist.name,
-        album: song?.album?.name,
-        albumArt: song?.album?.image,
-        releaseDate: song?.releasedAt,
-        image: song?.image,
-      });
-      response.headers.set("Content-Type", "application/json");
-      response.headers.set("Access-Control-Allow-Origin", "*");
-      response.headers.set(
-        "Cache-Control",
-        "public, s-maxage=86400, stale-while-revalidate=43200"
-      );
-      return response;
+    const searches = await client.songs.search(
+      `${decodeURIComponent(title as string)} ${decodeURIComponent(
+        title.length > 1 ? (artist as string) : ""
+      )}`
+    );
+    const song = searches[0];
+    const lyrics = await song?.lyrics();
 
-    } else {
-      return NextResponse.json({ error: "Bad request" });
-    }
-  }
-  catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: "Internal server error" });
+    const response = NextResponse.json({
+      lyrics: lyrics,
+      title: song?.title,
+      artist: song?.artist.name,
+      album: song?.album?.name,
+      albumArt: song?.album?.image,
+      releaseDate: song?.releasedAt,
+      image: song?.image,
+    });
+    response.headers.set("Content-Type", "application/json");
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set(
+      "Cache-Control",
+      "public, s-maxage=86400, stale-while-revalidate=43200"
+    );
+    return response;
+
+  } else {
+    return NextResponse.json({ error: "Bad request" });
   }
 }
+
+
 
