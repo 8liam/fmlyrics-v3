ad17dde5a4f3d70a90ac9e02bd7b8c0cb2894664
Liam <n11102764@qut.edu.au>
2024-03-03 02:56:23 +1000

    new: profile page started

---
+++ app/profile/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import Navbar from "@/app/components/navbar";
+import { useSession } from "next-auth/react";
+
+
+export default function Profile() {
+    const { data, status } = useSession();
+    console.log(data);
+    return (
+        <>
+            <Navbar />
+            {status === "authenticated" ? (
+                <div className="xl:px-[25vw] lg:px-[15vw] md:px-[5vw] px-[2vw] my-4">
+                    <div className="bg-primary rounded p-2">
+                        <h1 className="text-2xl font-bold">{data?.user?.name}</h1>
+                        <p className="text-sm">{data?.user?.email}</p>
+                    </div>
+                </div>
+            ) : (
+                <div className="xl:px-[25vw] lg:px-[15vw] md:px-[5vw] px-[2vw] my-4">
+                    <div className="bg-primary rounded p-2">
+
+                        <h1 className="text-2xl font-bold">Not Authenticated</h1>
+                    </div>
+                </div>
+            )}
+        </>
+
+    );
+
+}
+
